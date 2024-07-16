7f23a341d53e2e8d889af510c214818078bf978b
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-07-16 23:55:24 +0530

    home page

---
+++ assets/main.js
@@ -0,0 +1,54 @@
+//Custom JS
+    
+        // jQuery for Navbar active class
+        $(document).ready(function () {
+            $('.navbar-nav .nav-item .nav-link').click(function () {
+                $('.navbar-nav .nav-item .nav-link').removeClass('active');
+                $(this).addClass('active');
+            });
+        });
+
+        // jQuery for smooth scrolling
+        $('.navbar-nav .nav-link').click(function (e) {
+            if (this.hash !== '') {
+                e.preventDefault();
+
+                const hash = this.hash;
+
+                $('html, body').animate(
+                    {
+                        scrollTop: $(hash).offset().top,
+                    },
+                    800,
+                    function () {
+                        window.location.hash = hash;
+                    }
+                );
+            }
+        });
+
+        // jQuery for Carousel
+        $('.carousel').carousel({
+            interval: 2000,
+        });
+   // Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+/*function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+*/
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
