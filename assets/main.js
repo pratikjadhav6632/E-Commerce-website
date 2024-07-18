37583619ca7e943d2dba71ddcbfccf016d6c794f
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-07-18 16:11:44 +0530

    product page

--- assets/main.js
+++ assets/main.js
@@ -31,27 +31,28 @@
         $('.carousel').carousel({
             interval: 2000,
         });
-   // Get the button
-let scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    scrollFunction();
-};
+    // Get the button
+    var goToTopBtn = document.getElementById("goToTopBtn");
 
-/*function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollToTopBtn.style.display = "block";
-    } else {
-        scrollToTopBtn.style.display = "none";
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopBtn.style.display = "block";
+        } else {
+            goToTopBtn.style.display = "none";
+        }
     }
-}
-*/
-// When the user clicks on the button, scroll to the top of the document
-function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
-//Product page
+    // When the user clicks on the button, scroll to the top of the document
+    goToTopBtn.onclick = function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    };
+
+
 
