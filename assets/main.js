bbb53bc9bf8140eb811c81eb7e48d0a1e4b78821
Developerpratikjadhav <pratikjadhav6632@gmail.com>
2024-07-19 19:34:02 +0530

    initial commit

--- assets/main.js
+++ assets/main.js
@@ -54,6 +54,32 @@
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
     };
 
+    
+    $(document).ready(function () {
+        updateCart();
+        const cartCount = JSON.parse(localStorage.getItem('cart'))?.length || 0;
+        $('#cart-count').text(cartCount);
+    });
+
+
+
+    function changeQuantity(productId, delta) {
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+        let item = cart.find(item => item.id === productId);
+        if (item) {
+            item.quantity = Math.max(1, item.quantity + delta);
+            localStorage.setItem('cart', JSON.stringify(cart));
+            updateCart();
+        }
+    }
+
+    function removeFromCart(productId) {
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+        cart = cart.filter(item => item.id != productId);
+        localStorage.setItem('cart', JSON.stringify(cart));
+        updateCart();
+    }
+
     $(document).ready(function () {
         updateCart();
         const cartCount = JSON.parse(localStorage.getItem('cart'))?.length || 0;
